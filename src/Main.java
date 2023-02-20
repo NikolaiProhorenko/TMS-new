a8f2b3cd0387fd04430941c045d2f44db60809c5
NikolaiProhorenko <kolman2008@mail.ru>
2023-02-21 00:12:31 +0300

    DZ new

--- src/Main.java
+++ src/Main.java
@@ -1,169 +1,162 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        numbers();
-        ourTriangle();
-        getNumbers();
-        sumPositiveNumber();
-        sumPositiveAndNegativeNumber();
-        maxNumbers();
-        programmer();
+        //numbers();
+        //deliteNumber();
+        getRandomerNumer();
+        // minMaxArray();
     }
 
     static void numbers() {
 
-        // задача №1 В переменную записываем число. Надо вывести на экран сколько в этом числе цифр
-        // и положительное оно или отрицательное. Например, "это однозначное положительное число".
-        // Достаточно будет определить, является ли число однозначным, "двухзначным или трехзначным и более.
-        int number = 415;
+        //Создайте массив целых чисел. Напишете программу, которая выводит
+        //сообщение о том, входит ли заданное число в массив или нет. Пусть
+        //число для поиска задается с консоли (класс Scanner)
+        Scanner MyNumber = new Scanner(System.in);
 
-        if (number == 0)
-            System.out.println("Our number: 0 " + number);
+        System.out.print("Enter number: ");
+        int[] number = new int[] {1,3,5,7,9};
+        int Number = MyNumber.nextInt();
+        int ArrayElement;
+        int place=0;
 
-        else if  (number<=-9 && number<0)
-            System.out.println("Number negative, unambiguous our number:: " + number);
-
-        else if (number<=-99 && number<=-10)
-            System.out.println("Number negative, two-digit our number:: " + number);
-
-        else if (number<=-999 && number<=-100)
-            System.out.println("Number negative, three-digit our number:: " + number);
-
-        else if (number>=0 && number<=9)
-            System.out.println("Number unambiguous our number:: " + number);
-
-        else if (number>=10 && number<=100)
-            System.out.println("Number two-digit our number: " + number);
-
-        else if (number>=100 && number<=999)
-            System.out.println("Number three-digit our number: " + number);
-        System.out.println();
-    }
-
-    static void ourTriangle()
-    {
-        //Треугольник существует только тогда, когда сумма любых двух его сторон
-        //больше третьей. Определить существует ли такой треугольник. Дано: a, b, c –
-        //стороны предполагаемого треугольника. Требуется сравнить длину каждого
-        //отрезка-стороны с суммой двух других. Если хотя бы в одном случае отрезок
-        //окажется больше суммы двух других, то треугольника с такими сторонами не
-        //существует.
-
-        int a=5;
-        int b=6;
-        int c=11;
-
-        if ((a+b>=c) && (a+c>=b) && (b+c>=a))
-            System.out.printf("Our triangle a=%d, b=%d, c=%d may live\n",a,b,c);
-        else
-            System.out.printf("Our triangle a=%d, b=%d, c=%d can't live\n",a,b,c);
-        System.out.println();
-    }
-
-    static void getNumbers()
-    {
-        //Дано целое число. Если оно является положительным, то прибавить к нему 1.
-        //Если отрицательным, то вычесть из него 2. Если нулевым, то заменить его на 10. Вывести полученное число.
-
-        int number = 5;
-        if (number > 0) {
-            number++;
-            System.out.println("Get number: " + number);
-        } else if (number < 0) {
-            number -= 2;
-            System.out.println("Get number: " + number);
-        } else {
-            number = 10;
-            System.out.println("Get number: " + number);
-        }
-        System.out.println();
-
-
-    }
-    static void sumPositiveNumber()
-    //Даны 3 целые числа. Найти количество положительных чисел в исходном наборе.
-    {
-        int number1 = 5;
-        int number2 = -6;
-        int number3 = 1;
-        int sum =0;
-
-        if (number1 >0) {
-            sum ++;
-        }
-        if (number2 >0) {
-            sum ++;
-        };
-        if (number3>0) {
-            sum++;
-        };
-        System.out.println("Summa our numbers: " + sum);
-        System.out.println();
-
-
-
-    }
-
-    static void sumPositiveAndNegativeNumber()
-    //Даны 3 целых числа. Найти количество положительных и отрицательных
-    //чисел в исходном наборе.
-    {
-
-        int number1 = 5;
-        int number2 = -6;
-        int number3 = 1;
-        int sumPositive = 0;
-        int sumNegative = 0;
-
-        if (number1 >0) {
-            sumPositive++;
-        } else {
-            sumNegative++;
-        }
-        if (number2 >0) {
-            sumPositive++;
-        } else {
-            sumNegative++;
-        }
-        if (number3>0) {
-            sumPositive++;
-        } else {
-            sumNegative++;
+        for (int index = 0; index < number.length; index++) {
+            ArrayElement = number[index];
+            if (ArrayElement == Number)
+                place++;}
+        if (place>0){
+            System.out.println("Число входит в массив");}
+        else {
+            System.out.println("Число не входит в массив");
+            //      MyNumber.close();
         }
 
-        System.out.println("Sum our positive numbers: " + sumPositive);
-        System.out.println("Sum our negative numbers: " + sumNegative);
-        System.out.println();
     }
-    static void maxNumbers()
-    {
-        //Даны 2 числа. Вывести большее из них.
-        int number1 = 555;
-        int number2 = 85;
 
-        System.out.println("Biggest number is: " +((number1 > number2) ? number1: number2));
+    static void deliteNumber() {
+        //Создайте массив целых чисел. Удалите все вхождения
+        // заданного числа из массива.
+        //Пусть число задается с консоли (класс Scanner). Если такого числа нет
+        //- выведите сообщения об этом.
+        //В результате должен быть новый массив без указанного числа.
+
+        Scanner numbers = new Scanner(System.in);
+        System.out.print("Введите размер массива :");
+        int base = numbers.nextInt();
+        int[] array = new int[base];
+
+        System.out.println("Введите элементы массива :");
+        for (int a = 0; a < base; a++)
+        {
+            array[a] = numbers.nextInt();
+        }
+        System.out.print("Введите элемент который хотите удалить :");
+        int deliteNumber = numbers.nextInt();
+        int newBase = 0;
+        for (int a = 0; a < base; a++)
+        {
+            if (array[a] != deliteNumber)
+            {
+                array[newBase++] = array[a];
+            }
+        }
+        if (newBase == 0)
+        {
+            System.out.println("Массив пустой");
+        } else
+        {
+            System.out.print("Массив без числа равен :");
+            for (int a = 0; a < newBase; a++)
+            {
+                System.out.print(array[a] + " ");
+            }
+            //  numbers.close();
+
+        }
+
+    }
+
+    static void getRandomerNumer() {
+        //Cоздайте и заполните массив случайным числами и выведете
+        //максимальное, минимальное и среднее значение.
+        //Для генерации случайного числа используйте метод Math.random().
+        //Пусть будет возможность создавать массив произвольного
+        //размера. Пусть размер массива вводится с консоли.
+
+        Scanner numbe = new Scanner (System.in);
         System.out.println();
+        System.out.println("Print numbers array :");
+
+        int base = numbe.nextInt();
+        Random newNumbers = new Random();
+        {
+            int[] ar = new int[base];
+            for (int a = 0; a < base; a++)
+            {
+                ar [a] = newNumbers.nextInt(10);
+                System.out.println();
+                System.out.println("Our number "+ ar[a]);
+
+                {
+                    int min = ar[a];
+                    {
+                        min = Math.min(min, ar[a]);
+                        System.out.println("Min array number is: " + min);}
+
+                    int max = ar[a];
+                    {
+                        max = Math.max(max, ar[a]);
+                        System.out.println("Max array number is: " + max);}
+
+                    int average = 0;
+                    {
+                        average += ar[a];
+                        double Average = (double) average / base;
+                        System.out.println("Average numbers is: " + Average);}
+                    System.out.println();
+                    numbe.close();
+                }
+            }
+        }
+
     }
-    static void programmer()
-    // (Дополнительно) В переменную записываете количество программистов. В
-    //зависимости от количества программистов необходимо вывести правильно
-    //окончание. Например:
-    //• 2 программиста
-    //• 1 программиста
-    //• 10 программистов
-    //• и т.д
-    {
-        int programmer = 11;
-        if (programmer==1)
-        {System.out.println("У нас один программист");}
-        else if (programmer > 1 && programmer <= 5)
-        {System.out.printf("У нас %d программиста", programmer);}
-        else if (programmer >5 && programmer <=20 )
-        {System.out.printf("У нас %d программистов", programmer);}
-        else if (programmer == 21 )
-        {System.out.printf("У нас %d программист", programmer);}
-        else if (programmer >21 && programmer <=30 )
-        {System.out.printf("У нас %d программиста", programmer);}
-        else if (programmer >31 )
-        {System.out.print("Столько программистов вместе не собираються =) ");}
+    static void minMaxArray(){
+        //Создайте 2 массива из 5 чисел.
+        //Выведите массивы на консоль в двух отдельных строках.
+        //Посчитайте среднее арифметическое элементов каждого массива и
+        //сообщите, для какого из массивов это значение оказалось больше (либо
+        //сообщите, что их средние арифметические равны).
+
+        System.out.println();
+        System.out.print("Enter our array :");
+
+        int[] line1 = {5,12,14,8,};
+        int[] line2 = {22,5,4,9,17};
+        System.out.println();
+        System.out.println("We take array №1 " + Arrays.toString(line1));
+        System.out.println("We take array №2 "+ Arrays.toString(line2));
+        int arrays1 = 0 ;
+        int arrays2 = 0 ;
+        for (int a = 0; a < line1.length; a++)
+        {
+            arrays1 = line1[a];
+            arrays2 = line2[a];
+        }
+        arrays1 /= line1.length;
+        arrays2 /= line2.length;
+        if (arrays1 == arrays2)
+        {
+            System.out.println("Array №1 = Array №2");}
+        else if
+        (arrays1 > arrays2)
+        { System.out.println("Array №1 > Array №2");}
+        else if
+        (arrays1 < arrays2)
+        {System.out.println("Array №1 < Array №2");}
+
     }
+
 }
