613942c2f894f0bd693111cbc4454149aa73e428
NikolaiProhorenko <kolman2008@mail.ru>
2023-02-21 02:45:14 +0300

    DZ new

---
+++ src/DzLesson_6/CreditCard.java
@@ -0,0 +1,37 @@
+package DzLesson_6;
+
+import java.util.Scanner;
+
+public class CreditCard {
+    int numberAcaunt;
+    double acauntSum;
+
+    public CreditCard (int cardNumber, double cardAmount)
+    {
+        numberAcaunt = cardNumber;
+        acauntSum = cardAmount;
+    }
+    public void cachIn (int i) {
+        System.out.print ("Please, enter how mach you want cash in card with number "+numberAcaunt+": ");
+        Scanner Cash = new Scanner (System.in);
+        double Cash2 = Cash.nextDouble();
+        acauntSum =acauntSum + Cash2;
+    }
+
+    public void cashOut (int i) {
+        System.out.print ("Please, enter how mach you want cash out card with number "+numberAcaunt+": ");
+        Scanner Cash = new Scanner (System.in);
+        double Cash2 = Cash.nextDouble();
+        acauntSum = acauntSum - Cash2;
+        System.out.println();
+        Cash.close();}
+
+    public void acauntInfo() {
+        System.out.println ("The card account number is: "+ numberAcaunt);
+        System.out.println ("Card balance is "+acauntSum);
+        System.out.println();}
+
+}
+
+
+
---
+++ src/DzLesson_6/MyMain.java
@@ -0,0 +1,20 @@
+import DzLesson_6.CreditCard;
+
+public class MyMain {
+    public static void main(String[] args) {
+        //  1. get credit card info
+        DzLesson_6.CreditCard card1 = new CreditCard(1551551, 22);
+        DzLesson_6.CreditCard card2 = new CreditCard(8554555, 125);
+        DzLesson_6.CreditCard card3 = new CreditCard(155659, 2255);
+
+        card1.cachIn(0);
+        card2.cachIn(0);
+        card3.cashOut(0);
+
+
+        card1.acauntInfo();
+        card2.acauntInfo();
+        card3.acauntInfo();
+
+    }
+}
