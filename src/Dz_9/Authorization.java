c5eb5a79423766df9df79bf41ea1bf63fa475915
NikolaiProhorenko <kolman2008@mail.ru>
2023-03-12 22:37:17 +0300

    DZ 9

--- src/Dz_9/Authorization.java
+++ src/Dz_9/Authorization.java
@@ -1,7 +1,23 @@
 package Dz_9;
+    //Создать класс, в котором будет статический метод.
+    //Этот метод принимает на вход три параметра:
+    //Login,
+    //Password,
+    //confirmPassword.
+    //Все поля имеют тип данных String.
+    //Длина login должна быть меньше 20 символов и не должен содержать
+    //пробелы.
+    //Если login не соответствует этим требованиям, необходимо выбросить
+    //WrongLoginException.
+    //Длина password должна быть меньше 20 символов, не должен содержать
+    //пробелы и должен содержать хотя бы одну цифру.
+    //Также password и confirmPassword должны быть равны.
+    //Если password не соответствует этим требованиям, необходимо
+    //выбросить WrongPasswordException.
+    //Метод возвращает true, если значения верны или false в другом случае.
 public class Authorization {
 
-    public static boolean authorization(String login, String password, String repeatedPassword)
+    public static boolean authorization(String login, String password, String confirmPassword)
             throws WrongLoginException, WrongPasswordException {
 
         if (login.length() > 19 || login.contains(" ") || login.length() < 0 ) {
@@ -11,8 +27,8 @@ public class Authorization {
                  password.length() < 0 ) {
             throw new WrongPasswordException("Your password is -> WrongPasswordException"); }
         {
-        if (!password.equals(repeatedPassword)) {
-            throw new WrongPasswordException("Your repeated password is -> WrongPasswordException"); }}
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Your confirm password is -> WrongPasswordException"); }}
         return true;
     }
 
